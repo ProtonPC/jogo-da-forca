964c69a5b1c9af821c7137505b222c54718fc92d
Bruno Oliveira <bds.commus@gmail.com>
2023-09-18 13:15:19 -0300

    add helper function to convert int into char

--- src/Helpers/customHelper.php
+++ src/Helpers/customHelper.php
@@ -16,3 +16,16 @@ function dump($param): void
 {
     var_dump($param);
 }
+
+/**
+ * Convert the param $code to a char string
+ */
+function getCharacter(int $code): string
+{
+    if($code > 90) $code-=32;
+    if($code < 65 || $code > 90)
+    {
+        throw new Exception("The code must be between 65 and 90", 1);
+    }
+    return chr($code);
+}
