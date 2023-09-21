b4db60f1e5c9c3f578df9777b3877e72c4b50095
Bruno Oliveira <bds.commus@gmail.com>
2023-09-20 21:16:48 -0300

    create session helper class

---
+++ src/Helpers/Session.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Helpers;
+
+class Session
+{
+    public function __construct()
+    {
+        if (!session_id()) {
+            session_start();
+        }
+    }
+
+    public static function get(string $key): string
+    {
+        return self::has($key) ? $_SESSION[$key] : 'null';
+    }
+    public static function set(string $key, string $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function unset(string $key): void
+    {
+        if (self::has($key)) {
+            unset($_SESSION[$key]);
+        }
+    }
+
+    public static function has(string $key): bool
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    public static function isEmpty(string $key): bool
+    {
+        return self::has($key) && empty($_SESSION[$key]);
+    }
+
+    public static function destroy(): void
+    {
+        $_SESSION = [];
+    }
+}
