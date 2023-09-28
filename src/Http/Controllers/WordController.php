a31814e9a86c609020198301273e85ba39e9a52c
Bruno Oliveira <bds.commus@gmail.com>
2023-09-27 21:32:11 -0300

    add html table to words

--- src/Http/Controllers/WordController.php
+++ src/Http/Controllers/WordController.php
@@ -14,8 +14,28 @@ class WordController extends BaseController
 
     public function readAllWord()
     {
+        $words = [
+            [
+                "id" => 1,
+                "name" => "Primeira",
+                "tip" => "A palavra mais usada",
+                "level" => 1
+            ],
+            [
+                "id" => 2,
+                "name" => "Segunda",
+                "tip" => "Uma das palavras mais usadas",
+                "level" => 2
+            ],
+            [
+                "id" => 3,
+                "name" => "Terceira",
+                "tip" => "Uma das palavras menos usadas",
+                "level" => 3
+            ],
+        ];
         return $this->view('word/index.html', [
-            'user' => 'dados',
+            'words' => $words
         ]);
     }
 
