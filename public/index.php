eae84b85b5fa9b5a517428c81e436ff012498119
Bruno Oliveira <bds.commus@gmail.com>
2023-10-16 02:09:02 -0300

    making word works

--- public/index.php
+++ public/index.php
@@ -3,6 +3,10 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config/app.php';
 
+if (!session_id()) {
+    session_start();
+}
+
 use Pecee\SimpleRouter\SimpleRouter;
 
 SimpleRouter::setDefaultNamespace('src\Http\Controllers');
--- src/Http/Controllers/GameController.php
+++ src/Http/Controllers/GameController.php
@@ -2,24 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Session;
+use App\Services\WordService;
+
 class GameController extends BaseController
 {
     public function index()
     {
-        $words = [
-            'abacate',
-            'banana',
-            'pera',
-            'manga',
-            'laranja',
-            'uva',
-            'morango',
-        ];
-
-        $word = $words[rand(0, count($words) - 1)];
+        $word = WordService::getCurrentWord();
 
         return $this->view('game/index.html', [
             'word' => $word,
         ]);
+    }
+
+    public function play($letter)
+    {
+        $word = WordService::getCurrentWord();
+
+        foreach ($word['letters'] as $key => $value) {
+            if (Session::has('letter' . $key) && Session::get('letter' . $key) === strtolower($letter)) {
+                $word['letters'][$key] = strtolower($letter);
+            }
+        }
+
+        Session::set('current_word_encoded', implode($word['letters']));
+
+        // $currentWordEncoded = Session::get('current_word_encoded');
+        // $word['encoded'] = str_split($word['encoded']);
+
+        // foreach ($word['encoded'] as $key => $letter) {
+        //     $word['encoded'][$key] = $word['letters'][$key];
+        // }
+        // Session::set('current_word_encoded', implode($word['encoded']));
+        // $word['encoded'] = array_map(function ($letter) {
+        //     return $letter === '_' ? $letter : strtolower($letter);
+        // }, str_split($word['encoded']));
+
+        // $word['encoded'] = implode($word['encoded']);
+
+        return $this->view('game/index.html', [
+            'word' => $word,
+        ]);
+    }
+
+    public function resetGame()
+    {
+        WordService::unsetCurrentWord();
+        header('Location: /');
     }
 }
